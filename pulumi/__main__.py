ed27f715a8890c655ea02ca326864c27743f1545
Febrian Setianto <febrian.setianto@gmail.com>
2022-06-13 00:37:19 +0300

    wrap bastion creation in a function

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -29,44 +29,55 @@ network = networking.Network(network_name)
 
 # bastion
 
-bastion_secgroup = networking.SecGroup(
-    "bastion",
-    name="bastion",
-    description="Bastion node",
-    delete_default_rules=True,
-)
 
-bastion_secgroup_ingress = networking.SecGroupRule(
-    "bastion_secgroup_ingress",
-    direction="ingress",
-    ethertype="IPv4",
-    protocol="tcp",
-    port_range_min=22,
-    port_range_max=22,
-    remote_ip_prefix="0.0.0.0/0",
-    security_group_id=bastion_secgroup.id,
-)
+def bastion():
 
-bastion_secgroup_egress = networking.SecGroupRule(
-    "bastion_secgroup_egress",
-    direction="egress",
-    ethertype="IPv4",
-    remote_ip_prefix="0.0.0.0/0",
-    security_group_id=bastion_secgroup.id,
-)
+    # security group
+    bastion_secgroup = networking.SecGroup(
+        "bastion",
+        name="bastion",
+        description="Bastion node",
+        delete_default_rules=True,
+    )
 
-bastion = compute.Instance(
-    "bastion",
-    name="bastion",
-    image_name=config.require("image_name"),
-    flavor_name=config.require("flavor_bastion"),
-    key_pair=cluster_keypair.name,
-    security_groups=[secgroup, bastion_secgroup],
-    networks=[compute.InstanceNetworkArgs(name=network_name)],
-)
+    # security group rules
+    networking.SecGroupRule(
+        "bastion_secgroup_ingress",
+        direction="ingress",
+        ethertype="IPv4",
+        protocol="tcp",
+        port_range_min=22,
+        port_range_max=22,
+        remote_ip_prefix="0.0.0.0/0",
+        security_group_id=bastion_secgroup.id,
+    )
 
-bastion_fip = compute.FloatingIpAssociate(
-    "bastion",
-    floating_ip=config.require("floating_ip_bastion"),
-    instance_id=bastion.id,
-)
+    networking.SecGroupRule(
+        "bastion_secgroup_egress",
+        direction="egress",
+        ethertype="IPv4",
+        remote_ip_prefix="0.0.0.0/0",
+        security_group_id=bastion_secgroup.id,
+    )
+
+    # compute instance
+    bastion = compute.Instance(
+        "bastion",
+        name="bastion",
+        image_name=config.require("image_name"),
+        flavor_name=config.require("flavor_bastion"),
+        key_pair=cluster_keypair.name,
+        security_groups=[secgroup, bastion_secgroup],
+        networks=[compute.InstanceNetworkArgs(name=network_name)],
+    )
+
+    # and assign a floating IP
+    compute.FloatingIpAssociate(
+        "bastion",
+        floating_ip=config.require("floating_ip_bastion"),
+        instance_id=bastion.id,
+    )
+
+
+if __name__ == "__main__":
+    bastion()
